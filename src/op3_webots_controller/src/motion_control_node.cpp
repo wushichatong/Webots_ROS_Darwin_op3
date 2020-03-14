4454ccad16a5b7c3ef6615151a3cf21cda78f3bf
wushichatong <wushichatong@github.com>
2020-03-14 17:22:20 +0800

    init push

---
+++ src/op3_webots_controller/src/motion_control_node.cpp
@@ -0,0 +1,78 @@
+#include <webots_interface.h>
+#include <ros/ros.h>
+#include <op3_kinematics_dynamics/op3_kinematics_dynamics.h>
+#include <robotis_math/fifth_order_polynomial_trajectory.h>
+
+robotis_op::OP3KinematicsDynamics* op3_kd_;
+double leg_angle[12];
+using namespace std;
+
+bool standInit(const double T);
+enum {STAND_INIT, STANDING, WALKING};
+int robot_status = STAND_INIT;
+webots_interface webots_interface_;
+int main(int argv, char **argc)
+{
+
+    ros::init(argv, argc, "motion_control_node");
+    ros::NodeHandle* rosnode_ = new ros::NodeHandle();
+    double right_leg_angle[6];
+    double left_leg_angle[6];
+    webots_interface_.Init();
+//    double leg_angle[12];
+    op3_kd_ = new robotis_op::OP3KinematicsDynamics(robotis_op::WholeBody);
+    ros::Rate loop_rate(1000);
+    loop_rate.sleep();
+
+    while(ros::ok()){
+      switch(robot_status){
+      case STAND_INIT:
+        standInit(0.5);
+        break;
+      case WALKING:
+        break;
+      }
+      webots_interface_.Write(leg_angle);
+//      webots_interface_.Read(leg_angle);
+      loop_rate.sleep();
+      ros::spinOnce();
+    }
+
+
+    return 0;
+
+}
+
+bool standInit(const double T){
+  double right_leg_angle[6];
+  double left_leg_angle[6];
+  static double time_count = 0.0;
+  time_count += 0.001;
+  cout<<time_count<<endl;
+  robotis_framework::FifthOrderPolynomialTrajectory* a= new robotis_framework::FifthOrderPolynomialTrajectory(0, -0.2505,0,0,T,-0.2005,0,0);
+//  robotis_framework::FifthOrderPolynomialTrajectory* b= new robotis_framework::FifthOrderPolynomialTrajectory(0,0,0,0,T,-0.2,0,0);
+  if (op3_kd_->calcInverseKinematicsForRightLeg(&right_leg_angle[0], 0.00, 0.0, a->getPosition(time_count), 0, 0, 0) == false)
+  {
+    printf("1 IK not Solved EPR : %f %f %f %f %f %f\n", 0.0, 0.0, a->getPosition(time_count), 0.0, 0.0, 0.0);
+    return false;
+  }
+
+  if (op3_kd_->calcInverseKinematicsForLeftLeg(&left_leg_angle[0], 0.00, -0.00, a->getPosition(time_count), 0, 0, 0) == false)
+  {
+    printf("2 IK not Solved EPR : %f %f %f %f %f %f\n", 0.0, 0.0, a->getPosition(time_count), 0.0, 0.0, 0.0);
+    return false;
+  }
+
+  for (int i=0; i<12; i++) {
+    if(i%2==0)
+      leg_angle[i] = right_leg_angle[i/2];
+    else {
+      leg_angle[i] = left_leg_angle[i/2];
+    }
+  }
+  if(time_count>T)
+    robot_status = WALKING;
+
+
+
+}
---
+++ src/op3_webots_controller/src/webots_interface.cpp
@@ -0,0 +1,88 @@
+#include "webots_interface.h"
+
+
+using namespace std;
+using namespace webots;
+static const char *motorNames[20] = {
+  "ShoulderR" /*ID1 */, "ShoulderL" /*ID2 */, "ArmUpperR" /*ID3 */, "ArmUpperL" /*ID4 */, "ArmLowerR" /*ID5 */,
+  "ArmLowerL" /*ID6 */, "PelvYR" /*ID7 */,    "PelvYL" /*ID8 */,    "PelvR" /*ID9 */,     "PelvL" /*ID10*/,
+  "LegUpperR" /*ID11*/, "LegUpperL" /*ID12*/, "LegLowerR" /*ID13*/, "LegLowerL" /*ID14*/, "AnkleR" /*ID15*/,
+  "AnkleL" /*ID16*/,    "FootR" /*ID17*/,     "FootL" /*ID18*/,     "Neck" /*ID19*/,      "Head" /*ID20*/
+};
+
+#define ID_R_LEG_START  (6)
+#define ID_L_LEG_START  (7)
+webots_interface::webots_interface()
+{
+
+}
+webots_interface::~webots_interface()
+{
+
+}
+bool webots_interface::Init(){
+
+
+    cout<<"init:"<<endl;
+//    wb_robot_init();
+
+//    time_step=1;
+
+//    //gait::MotorGaitApi::RHipPitchMotor
+//    // motor
+//    RHipYawMotor = wb_robot_get_device(motorNames[0]);
+//    wb_motor_enable_torque_feedback(RHipYawMotor, time_step);
+////    wb_position_sensor_enable(RHipYawMotor, time_step);
+////wb_position_sensor_enable()
+
+//    wb_motor_set_position(RHipYawMotor, 1);
+//    cout<<wb_motor_get_target_position(RHipYawMotor)<<endl;
+//    wb_robot_step(1);
+//    webots::Robot robot = new Robot();
+//    webots::Robot* robot = new webots::Robot();
+    mTimeStep = getBasicTimeStep();
+//    cout<<"0 value:";
+//    webots::Motor* ShoulderRMotor = getMotor("ShoulderR");
+//    webots::PositionSensor* ShoulderRPositionSensor = ShoulderRMotor->getPositionSensor();
+//    ShoulderRPositionSensor->enable(mTimeStep);
+//    cout<<"1 value:"<<ShoulderRPositionSensor->getValue()<<endl;
+//    ShoulderRMotor->setPosition(1);
+//    webots::LED* headLed = getLED("HeadLed");
+//    headLed->set(0xFF00FF);
+//    step(mTimeStep);
+//    cout<<"2 value:"<<ShoulderRPositionSensor->getValue()<<endl;
+
+    for (int i = 0; i < NMOTORS; i++) {
+      string sensorName = motorNames[i];
+      getMotor(sensorName)->setPosition(0);
+      sensorName.push_back('S');
+      getPositionSensor(sensorName)->enable(mTimeStep);
+    }
+
+    step(mTimeStep);
+
+
+}
+bool webots_interface::Read(double* angle){
+//  cout<<wb_motor_get_target_position(RHipYawMotor)<<endl;
+  cout<<"into read"<<endl;
+  for (int i = 0; i < NMOTORS; i++) {
+    string sensorName = motorNames[i];
+    sensorName.push_back('S');
+//    getPositionSensor(sensorName)->enable(mTimeStep);
+    cout<<getPositionSensor(sensorName)->getValue()<<endl;
+//    cout<<angle[i];
+  }
+
+}
+bool webots_interface::Write(double* angle){
+//  wb_motor_set_position(RHipYawMotor, 1);
+  for (int i = 0; i < 12; i++) {
+    string sensorName = motorNames[ID_R_LEG_START + i];
+    getMotor(sensorName)->setPosition(angle[i]);
+  }
+  step(mTimeStep);
+  cout<<"write succ"<<endl;
+
+}
+
